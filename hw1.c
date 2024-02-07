3b016d4c0309a60e089b15a5beeed056f9f58e09
Edan Weinberg <edanweinberg@mail.tau.ac.il>
2024-02-07 14:54:24 +0200

    started to insert points(changed a comment)

--- hw1.c
+++ hw1.c
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
     }
 
 
-    insertPointsFromStdin(points); // SOMETHING WRONG
+    insertPointsFromStdin(points);
 
     // Insert first k points as centroids
     for (int i = 0 ; i < k ; i++){
