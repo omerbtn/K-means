414f0aa8f0dd5f18ec442f20961bc5782649b6d3
Edan Weinberg <edanweinberg@mail.tau.ac.il>
2024-02-08 20:23:35 +0200

    after workshop

--- hw1.c
+++ hw1.c
@@ -1,43 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #define     ITER_DEFAULT    200
 #define     epsilon         0.001
 
+// Assisting methods
+void    insertPointsFromStdin(double**, int, int);
 int     checkLegal(int, int, int, int);
+int     findClosestCentroid(double*, double**);
+void    updatePoints(double**, double**, int*, int);
 
-/*
-    TO DO LIST
-    create a points malloc array
-    create a centroid malloc array
-    for loop of points
-    
-
-    calculating distance of d dimension
-
-*/
 
 int main(int argc, char *argv[])
 {
     // consts
-
     int k, n, d, iter; // consts
     double **points; // A list of vectors
     double **centroids; // A list of vectors
-    int i; // for the for loops
-
-    // Assisting methods
-    void insertPointsFromStdin(double**, int, int);
-    int checkLegal(int, int, int, int);
-
+    int *cluster_index; // A list of clusters
+    int i, j; // for the for loops
 
     // initializing and checking legality
     if (argc > 5 || argc < 4){
         printf("\nToo little/many arguments in command line!");
         return 0;
     }
-
     if (argc == 4){
         iter = ITER_DEFAULT;
     } else {
@@ -49,7 +38,6 @@ int main(int argc, char *argv[])
     if (checkLegal(k,n,d,iter) == 0){
         return 0;
     }
-    printf("%d\n%d\n%d\n%d\nTESTING", k, n, d, iter);
 
     // Allocate memory for empty array of points
     // (represented as 'n' d-sized arrays)
@@ -65,65 +53,115 @@ int main(int argc, char *argv[])
     // NEED ASSERT OF ENOUGH SPACE
 
     centroids = calloc(k, sizeof(double*));
-    for (i = 0; i < k ; i++) {
-        points[i] = calloc(d, sizeof(double));
+    for (i = 0 ; i < k ; i++) {
+        centroids[i] = calloc(d, sizeof(double));
     }
 
     insertPointsFromStdin(points, n, d);
 
     // Insert first k points as centroids
-    for (int i = 0 ; i < k ; i++){
-        centroids[i] = points[i];
+    for (i = 0 ; i < k ; i++){
+        memcpy(centroids[i], points[i], sizeof(double) * d);
     }
 
-    // FREE EVERYTHING BEFORE !
+    cluster_index = calloc(n,sizeof(int));
+
+    
+    // go over k- for each cluster(0 - k-1) calculate new centroid and compare
+    for (i = 0 ; i < iter ; i++){
+        updatePoints(points, centroids, cluster_index, n);
+
+
+
+        if (updateCentroids(
+            centroids, cluster_index, points, n, d, k) == 1){
+            
+            // leave because of epsilon or something
+        }
+        
+        
+    }
+
+    // if less than epsilon terminate like python
+
+
+    // FREE TWO DIMENSION ARRAYS PROPERLY WITH FOR
+    free(points);
+    free(centroids);
+    free(cluster_index);
     return 0;
 }
 
+
+int updateCentroids(
+    double** centroids, int* cluster_index, double** points, 
+    int n, int d, int k){
+
+    double* new_centroid;
+    int i, j, x, cnt;
+    
+
+    new_centroid = calloc(d, sizeof(double));
+
+    for (i = 0 ; i < k ; i++){
+        cnt = 0;
+        for (j = 0 ; j < n ; j++){
+            if (cluster_index[j] == i){
+                for (x = 0 ; x < d ; x++){
+                    new_centroid[x] += points[j][x];
+                }
+                cnt++;
+            }
+
+        }
+
+        for (x = 0 ; x < d ; x++){
+            new_centroid[x] /= cnt; // WORKS?
+        }
+
+        // also update centroids[i]
+        // return the check with the epsilon if converged
+        
+
+    }
+
+
+    return 0;
+}
+
+
+
+
+
+
+// DEAL WITH THIS
+int findClosestCentroid(double* point, double** centroids){
+    return 0;
+}
+
+// assign every point- an index of closest cluster
+void updatePoints(
+    double** points, double** centroids, int* cluster_index, int n){
+    
+    int j;
+    for (j = 0 ; j < n ; j++){
+        cluster_index[j] = findClosestCentroid(points[j], centroids);
+    }
+}
+
+
 // Gets the points array and fills it according to input
 void insertPointsFromStdin(double **points, int n, int d){
-    char *line = NULL;
-    size_t len = 0;
-    int curr_in_temp;
-    double *p = points[0];
-    char *temp = NULL;
     int i, j; // for loops
     double double_val;
-
     for (i = 0 ; i < n ; i++){
         for (j = 0 ; j < d - 1 ; j++){
-            scanf("%f,", &double_val);
+            scanf("%lf,", &double_val);
             points[i][j] = double_val;
         }
-        scanf("%f", &double_val);
+        scanf("%lf", &double_val);
         points[i][d - 1] = double_val;
     }
-
-
-    /*
-    for (i = 0 ; i < n ; i++){
-        getline(&line, &len, stdin);
-        printf("Line is\n%s", line);
-        temp = malloc(sizeof(line));
-        // NEED ASSERT OF ENOUGH SPACE
-        
-        for (j = 0 ; j < d ; j++){
-            // parse line into points[i][j];
-            curr_in_temp = 0;
-            while (line[0] != ',' && line[0] != EOF && line[0] != '\n'){ 
-                temp[curr_in_temp++] = line[0]; // line++[0]?
-                line++;
-            }
-            line++;
-            temp[curr_in_temp] = '\0';
-            double_val = atof(temp);
-            points[i][j] = double_val;
-        }
-        free(temp); // ??
-        
-    }
-
-    */
 }
 
 // Done
